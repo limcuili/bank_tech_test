17a8815e6a555893a39e5b73b2ca57e696af0922
limcuili <limcuili@hotmail.com>
2018-09-12 13:35:19 +0100

    Added withdraw method in Account class

--- lib/account.rb
+++ lib/account.rb
@@ -5,13 +5,19 @@ class Account
   # rubocop:enable Style/Documentation
   attr_reader :balance
 
-  def initialize
-    @balance = 0
+  def initialize(balance = 0)
+    @balance = balance
   end
 
   def deposit(amount)
     raise ArgumentError, 'Unable to deposit negative amount' if amount.negative?
 
     @balance += amount.round(2)
+  end
+
+  def withdraw(amount)
+    raise ArgumentError, 'Please insert a positive amount' if amount.negative?
+
+    @balance -= amount.round(2)
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,3 +1,4 @@
+# rubocop: disable Metrics/BlockLength
 # frozen_string_literal: true
 
 require 'account'
@@ -33,4 +34,25 @@ describe Account do
       expect(account.balance).to eq 2
     end
   end
+
+  describe '#withdraw' do
+    let(:account_with_100) { described_class.new(100) }
+    it { is_expected.to respond_to(:withdraw).with(1).argument }
+
+    it 'decreases a positive amount of the balance' do
+      expect { account.withdraw(10) }.to change { account.balance }.by(-10)
+    end
+
+    it 'is unable to accept a negative withdrawl amount' do
+      expect { account.withdraw(-3) }.to raise_error(ArgumentError)
+    end
+
+    it 'shows error message when a negative withdrawal is made' do
+      expect { account.withdraw(-1) }.to raise_error(
+        'Please insert a positive amount'
+      )
+    end
+  end
 end
+
+# rubocop: enable Metrics/BlockLength
