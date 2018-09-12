160a45fc5b498dd740dff7f264c73fe731b7cf89
limcuili <limcuili@hotmail.com>
2018-09-12 13:20:04 +0100

    added deposit method to account

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,17 @@
+# rubocop: disable Style/Documentation
+# frozen_string_literal: true
+
 class Account
+  # rubocop:enable Style/Documentation
   attr_reader :balance
 
   def initialize
     @balance = 0
+  end
+
+  def deposit(amount)
+    raise ArgumentError, 'Unable to deposit negative amount' if amount.negative?
+
+    @balance += amount.round(2)
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -10,4 +10,27 @@ describe Account do
       expect(account.balance).to eq 0
     end
   end
+
+  describe '#deposit' do
+    it { is_expected.to respond_to(:deposit).with(1).argument }
+
+    it 'is adds a positive amount into the balance' do
+      expect { account.deposit(100) }.to change { account.balance }.by(100)
+    end
+
+    it 'is unable to accept a negative deposit' do
+      expect { account.deposit(-7) }.to raise_error(ArgumentError)
+    end
+
+    it 'shows error message when a negative deposit is made' do
+      expect { account.deposit(-1) }.to raise_error(
+        'Unable to deposit negative amount'
+      )
+    end
+
+    it 'rounds any amount to 2 decimal places' do
+      account.deposit(1.99999999)
+      expect(account.balance).to eq 2
+    end
+  end
 end
